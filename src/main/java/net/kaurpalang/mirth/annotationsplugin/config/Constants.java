086f98a2d4baa84cd57f5158855178d0781eb731
Kaur Palang <kaur.palang@protonmail.com>
2021-02-21 15:59:07 +0200

    Refactor and added a default

--- src/main/java/net/kaurpalang/mirth/annotationsplugin/config/Constants.java
+++ src/main/java/net/kaurpalang/mirth/annotationsplugin/config/Constants.java
@@ -1,14 +1,15 @@
-package net.kaurpalang.mirth.annotationsplugin;
+package net.kaurpalang.mirth.annotationsplugin.config;
 
 public class Constants {
     public static final String DEFAULT_AGGREGATOR_FILE_PATH = "distribution/aggregated.json";
 
     // Default Mirth plugin details
-    public static final String DEFAULT_PLUGIN_NAME = "Default plugin";
+    public static final String DEFAULT_PLUGIN_NAME = "Sample Plugin";
     public static final String DEFAULT_PLUGIN_AUTHOR = "John Doe";
     public static final String DEFAULT_PLUGIN_VERSION = "42";
     public static final String DEFAULT_MIRTH_VERSION = "3.10.1";
     public static final String DEFAULT_PLUGIN_URL = "https://github.com/kpalang/mirth-sample-plugin";
     public static final String DEFAULT_PLUGIN_DESCRIPTION = "A sample Mirth plugin to showcase my Maven plugin";
     public static final String DEFAULT_PLUGIN_PATH = "sampleplugin";
+    public static final String DEFAULT_PLUGIN_XML_OUTPUT_PATH = "${project.basedir}/plugin.xml";
 }
